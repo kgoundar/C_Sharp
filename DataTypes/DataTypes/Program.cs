213e858f40a1e2a04ae9ba66bc050673ba687d0d
Goundar <goundark@nala.roche.com>
2018-12-06 00:51:13 -0800

    learningC#

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -51,11 +51,65 @@ namespace DataTypes
 
 
 			//unsigned integer - ushot. Ranges from 0 to 6553
-			ushort i = 10000;
-			ushort j = 45;
+			//ushort i = 10000;
+			//ushort j = 45;
 
-			Console.WriteLine("The ushort value is " + i);
-			Console.WriteLine("The ushort value is " + j);
+			//Console.WriteLine("The ushort value is " + i);
+			//Console.WriteLine("The ushort value is " + j);
+			//Console.Read();
+
+			//long datatype ; signed long integer - ranges from -9223372036854775808 to 9223372036854775808
+			//long i = 100000000;
+			//long j = -45000000;
+
+			//Console.WriteLine("The long value is " + i);
+			//Console.WriteLine("The long value is " + j);
+			//Console.Read();
+
+
+			//Ulong datatype; unsigned long integer - range from 0 to 18446744073709551615
+			//ulong i = 100000000;
+			//ulong j = 45000000;
+
+			//Console.WriteLine("The ulong value is " + i);
+			//Console.WriteLine("The ulong value is " + j);
+			//Console.Read();
+
+
+			//Float Datatype; single precision floating point type - ranges from -3.402823e38 to 3.402823e38
+			//float i = 10.11F;
+			//float j = -45.44F;
+
+			//Console.WriteLine("The float value is " + i);
+			//Console.WriteLine("The float value us " + j);
+			//Console.Read();
+
+
+			//Double Datatyp; double precision floating point type - ranges from -1.79769313486232e308 to 1.79769313486232e308
+			//double i = 10.11111111;
+			//double j = -45.4444444;
+
+			//Console.WriteLine("The double value is " + i);
+			//Console.WriteLine("The double value is " + j);
+			//Console.Read();
+			//Console.Read();
+
+
+			//Char Datatype; Single unicode character
+			//char value = 'A';
+			//char value1 = 'a';
+
+			//Console.WriteLine("The char value is " + value);
+			//Console.WriteLine("The char value is " + value1);
+			//Console.Read();
+
+
+			//Bool Datatype; Used to define boolean value - either true or false
+			bool value = true;
+			bool value1 = false;
+
+			Console.WriteLine("The bool value is " + value);
+			Console.WriteLine("The bool vaule is " + value1);
 			Console.Read();
 
 		}
